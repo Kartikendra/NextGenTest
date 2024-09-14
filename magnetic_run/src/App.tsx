db1aad02564f194ddd9698eaf3107755315284e3
Kartikendra <kartikendrabhaskar@gmail.com>
2024-09-14 22:10:05 +0530

    initial commits

---
+++ magnetic_run/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Navbar } from './components/navbar/navbar'
+
+
+function App() {
+
+  return (
+  <>
+<Navbar/>
+  </>
+  )
+}
+
+export default App
---
+++ magnetic_run/src/components/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import './navbar.css'
+export const Navbar =()=>{
+    return(<>
+<div class="container">
+  <div class="navbar">
+    <h2>BrandName</h2>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+  </div>
+</div>
+    </>)
+}
