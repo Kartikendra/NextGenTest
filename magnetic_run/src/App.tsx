5de7f65607b6ee74762c3924a5b23a2cc6f72e8d
Kartikendra <kartikendrabhaskar@gmail.com>
2024-09-15 20:36:21 +0530

    skeleton added

--- magnetic_run/src/App.tsx
+++ magnetic_run/src/App.tsx
@@ -2,12 +2,30 @@
 import './App.css'
 import { Navbar } from './components/navbar/navbar'
 
-
+import NavItems from './configs/navItems.json'
 function App() {
 
   return (
   <>
-<Navbar/>
+  <div className='mainbody'>
+    <div className='navbar'>
+      {
+        NavItems.navItems.map((item)=>{
+          return(<div>
+              {item.label}
+          </div>
+        
+          )
+        })
+      }
+
+    </div>
+    <div className='content'>
+         <div className='maps'></div>
+         <div className='searchbar'></div>
+    </div>
+
+  </div>
   </>
   )
 }
